e1c21b700743959c4cadf2484fb46f7dc3259904
Pedro Arthur <pedroarthur.nf123@gmail.com>
2026-07-08 18:51:57 -0300

    Config usando variáveis de ambiente

--- config.py
+++ config.py
@@ -1,24 +1,10 @@
+import os
 from datetime import timedelta
-from app import app
 
 class Config:
-
-    # ==========================
-    # Banco de Dados
-    # ==========================
-
-    import os
-
-    app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv(
-        "DATABASE_URL"
-    )
-
+    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
-    # ==========================
-    # JWT
-    # ==========================
+    JWT_SECRET_KEY = os.getenv("JWT_SECRET_KEY")
 
-    JWT_SECRET_KEY = "minha_chave_de_api_julia"
-
-    JWT_ACCESS_TOKEN_EXPIRES = timedelta(days=365)
+    JWT_ACCESS_TOKEN_EXPIRES = timedelta(days=36)
